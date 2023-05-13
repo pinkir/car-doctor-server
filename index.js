27334012a3ec81a6696626a2cdda025211c645b9
pinkir <pinki.roy@outlook.com>
2023-05-14 02:02:07 +0600

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -27,6 +27,53 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const serviceCollection = client.db('carDoctor').collection('services');
+    const checkOutCollection = client.db('carDoctor').collection('checkouts');
+
+  //  get all service data
+    app.get('/services', async(req, res)=>{
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+  //  get 1 service data
+    app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+
+
+      // if you want to sort data
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1 },
+      };
+
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    })
+
+    // get checkout data
+
+    app.get('/checkouts', async(req, res)=>{
+      console.log(req.query.email)
+      const result = await checkOutCollection.find().toArray();
+      res.send(result)
+    })
+
+    //  insert a document (checkouts)
+    app.post('/checkouts', async(req, res)=>{
+      const checkOut = req.body;
+      // console.log(checkOut);
+      const result = await checkOutCollection.insertOne(checkOut);
+      res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
